3c0ee0ac82020a3c67c3531b21aa3c566f8e5e48
VeeraReddy <veerareddybogala16@gmail.com>
2024-07-09 21:16:35 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,13 +76,18 @@ document.querySelectorAll('.js-add-to-cart')
         matchingItem.quantity += 1;
       } else {
         cart.push({
-        productId: productId,
-        quantity: 1
-      });
-
+          productId: productId,
+          quantity: 1
+        });
       }
 
-      
-      console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
